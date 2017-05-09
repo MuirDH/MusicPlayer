66ba454345893968bf7d6242a2427ebacf94bb16
Muir Halleron <muir.halleron@gmail.com>
2017-05-09 15:37:25 +0100

    Change button functionality

---
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button play;
+    private Button pause;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (Button)findViewById(R.id.play);
+        play.setText(R.string.play_button);
+
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(),
+                        "Playing song", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                pause.setEnabled(true);
+                play.setEnabled(false);
+            }
+        });
+
+        pause = (Button)findViewById(R.id.pause);
+        pause.setText(R.string.pause_button);
+
+        pause.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),
+                        "Pausing song", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+                pause.setEnabled(false);
+                play.setEnabled(true);
+            }
+        });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.the_other_guys_well_sung_ep_04_desperado);
+
+    }
+}
