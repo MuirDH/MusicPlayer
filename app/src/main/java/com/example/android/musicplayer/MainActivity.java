9371312e5e9367647c5764342fa755ba2a67e125
Muir Halleron <muir.halleron@gmail.com>
2017-05-11 14:20:37 +0100

    Create releaseMediaPlayer function.

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.musicplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        releaseMediaPlayer();
 
         mediaPlayer = MediaPlayer.create(this, R.raw.the_other_guys_well_sung_ep_04_desperado);
 
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void performOnPause() {
-        
+
         Toast.makeText(getApplicationContext(),
                 "Pausing song", Toast.LENGTH_SHORT).show();
         mediaPlayer.pause();
@@ -67,13 +70,32 @@ public class MainActivity extends AppCompatActivity {
         play.setEnabled(false);
     }
 
-
     private void performOnEnd() {
 
         Toast.makeText(getApplicationContext(),
                 "Song finished", Toast.LENGTH_SHORT).show();
         pause.setEnabled(false);
         play.setEnabled(true);
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+
+            Log.v("MainActivity", "MediaPlayer has been released");
+        }
     }
 
 
